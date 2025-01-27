1d24a049bedae9efe85a3a3f3ec91e8128dbb81d
Valdis Saulespurens <38938337+ValRCS@users.noreply.github.com>
2025-01-27 12:50:57 +0000

    Hello Clojure

---
+++ clojure_projects/main.clj
@@ -0,0 +1,12 @@
+(ns main)
+
+(defn -main
+  []
+  (println "Hello, RBS!"))
+; So Clojure is a variant of Lisp, and it's a functional programming language.
+; LISP stands for List Processing, and it's the second oldest high-level programming language after Fortran.
+; Basica idea we are processing lists of data.
+; This data could be all sorts of things, but it's usually code.
+; so code and data are interchangeable in LISP.
+
+(-main)
