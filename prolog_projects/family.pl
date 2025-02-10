2149751b9b379eb3c5a00ff3e42cc4cd0a244e80
Valdis Saulespurens <38938337+ValRCS@users.noreply.github.com>
2025-02-10 13:06:40 +0000

    added more family facts

--- prolog_projects/family.pl
+++ prolog_projects/family.pl
@@ -1,4 +1,5 @@
 % Defining parent-child relationships
+parent(dino, albert).
 parent(albert, jim).
 parent(albert, peter).
 parent(jim, brian).
@@ -8,6 +9,10 @@ parent(peter, anna).
 grandparent(GP, GC) :- 
     parent(GP, P),      % GP is the parent of P
     parent(P, GC).      % P is the parent of GC
+% let's define great-grandparent relationship
+great_grandparent(GGP, GGC) :- 
+    parent(GGP, GP),    % GGP is the parent of GP
+    grandparent(GP, GGC). % GP is the grandparent of GGC
 % A rule to check if someone is a child of another
 child(C, P) :- 
     parent(P, C).
@@ -17,3 +22,10 @@ sibling(X, Y) :-
     parent(P, X), 
     parent(P, Y), 
     X \= Y.            % Ensure they aren't the same person
+
+% recursion can be used to define ancestor relationships
+ancestor(A, D) :- 
+    parent(A, D).       % A is a parent of D
+ancestor(A, D) :- 
+    parent(A, X),       % A is a parent of X
+    ancestor(X, D).     % X is an ancestor of D
